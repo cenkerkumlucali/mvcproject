34a68e3a4ef7d8b6a5517846d9553e46d2ca8120
Cenker <cenkerkumlucali0@gmail.com>
2021-05-30 17:11:07 +0300

    writer added

---
+++ MvcProje/Controllers/WriterController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Business.Concrete;
+using Business.ValidationRules;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using FluentValidation.Results;
+
+namespace MvcProje.Controllers
+{
+    public class WriterController : Controller
+    {
+        private WriterManager writerManager = new WriterManager(new EfWriterDal());
+        WriterValidator validator = new WriterValidator();
+        public ActionResult Index()
+        {
+            var writerValues = writerManager.GetList();
+            return View(writerValues);
+        }
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddWriter(Writer writer)
+        {
+            ValidationResult results = validator.Validate(writer);
+            if (results.IsValid)
+            {
+                writerManager.WriterAdd(writer);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var error in results.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName,error.ErrorMessage);
+                }
+            }
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult EditWriter(int id)
+        {
+            var writerValue = writerManager.GetById(id);
+            return View(writerValue);
+        }
+
+        [HttpPost]
+        public ActionResult EditWriter(Writer writer)
+        {
+            ValidationResult results = validator.Validate(writer);
+            if (results.IsValid)
+            {
+                writerManager.WriterUpdate(writer);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var error in results.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
